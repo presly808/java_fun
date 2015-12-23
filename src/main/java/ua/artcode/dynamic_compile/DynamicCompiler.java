0482547b202f58e118882f1949fb2143ccdf30b4
presly808 <presly808@gmail.com>
2015-12-23 20:51:27 +0200

    get right classpath

--- src/main/java/ua/artcode/dynamic_compile/DynamicCompiler.java
+++ src/main/java/ua/artcode/dynamic_compile/DynamicCompiler.java
@@ -6,6 +6,8 @@ import ua.artcode.utils.IOUtils;
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 import java.io.*;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.logging.Logger;
 
 /**
@@ -14,13 +16,27 @@ import java.util.logging.Logger;
 //TODO avoid static , add field root, resolve multithreading using stream
 public class DynamicCompiler {
 
+//    public static final String LIBS_CLASSPATH = "/home/serhii/dev/apache-tomcat-7.0.57/webapps/ROOT/WEB-INF/classes/";
+    public static final String LIBS_CLASSPATH = getLibsClasspath();
+
+    private static String getLibsClasspath(){
+        StringBuilder classPaths = new StringBuilder();
+        URL[] urls= ((URLClassLoader) (Thread.currentThread().getContextClassLoader())).getURLs();
+        for (URL url : urls) {
+            classPaths.append(url.getFile()).append(":");
+        }
+
+        return classPaths.toString();
+
+    }
+
     public DynamicCompiler() {}
 
     public String compile(String path) throws CompilationException {
         File sourceFile = new File(path); // TODO check if sourceFile redundant
         try {
             String absolutePath = sourceFile.getCanonicalPath();
-            Process pr = Runtime.getRuntime().exec(String.format("javac -cp /home/serhii/dev/apache-tomcat-7.0.57/webapps/ROOT/WEB-INF/classes/ %s",absolutePath));
+            Process pr = Runtime.getRuntime().exec(String.format("javac -cp " + LIBS_CLASSPATH + " %s",absolutePath));
 
             if (pr.waitFor() != 0){
                 String message = IOUtils.getMessage(new BufferedReader(new InputStreamReader(pr.getErrorStream())));
